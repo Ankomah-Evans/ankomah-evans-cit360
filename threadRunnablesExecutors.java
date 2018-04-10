480c77d1704e9ea5f577326ad2c372aa6953fd26
Ankomah-Evans <ankomahrich@gmail.com>
2018-04-10 11:59:31 -0600

    TER update

--- threadRunnablesExecutors.java
+++ threadRunnablesExecutors.java
@@ -1,12 +1,7 @@
-import android.os.Handler;
-import android.widget.TextView;
 
-import org.ter.json.JSONInputStream;
-import org.ter.json.JSONOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-
-import java.net.Socket;
-import java.util.HashMap;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -18,59 +13,49 @@ import java.util.HashMap;
  */
 public class threadRunnablesExecutors {
 
-    private final int messageSent;
-    private final String server;
-    private final Handler uiThreadStack;
-    private Object TextView;
+    public static void main(String args[]) {
 
-    public threadRunnablesExecutors(int currentNumMessagesSent, String aMessageForServer, Handler aUIThreadStack) {
-        this.messageSent = currentNumMessagesSent;
-        this.server = aMessageForServer;
-        this.uiThreadStack = aUIThreadStack;
+        RunnableDemo R1 = new RunnableDemo("Thread-1");
+        R1.start();
+        R1.run();
+
+        RunnableDemo R2 = new RunnableDemo("Thread-2");
+        R2.start();
+    }
+}
+
+class RunnableDemo implements Runnable {
+
+    private Thread t;
+    private String threadName;
+
+    RunnableDemo(String Name) {
+        String name = null;
+        threadName = name;
+        System.out.println("This creates thread" + threadName);
     }
 
-    @Override
     public void run() {
+        System.out.println("Running" + threadName);
         try {
-            Socket toServer = new Socket("10.0", 989);
-            final JSONInputStream inFromServer
-                    = new JSONInputStream(toServer.getInputStream());
-            final JSONOutputStream outToServer
-                    = new JSONOutputStream(toServer.getOutputStream());
-
-            HashMap<String, Object> commandMap = new HashMap<>();
-            commandMap.put("command", "Speak");
-            commandMap.put("count", this.messageSent);
-            commandMap.put("message", this.server);
-
-            outToServer.writeObject(commandMap);
-            System.out.println("waiting for response");
-            HashMap<String, Object> response = (HashMap<String, Object>) inFromServer.readObject();
-            System.out.println("response: " + response);
-            if (response.get("command").equals("Done")) {
-                final TextView responseView = TextView.get();
-                uiThreadStack.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (responseView != null) {
-                            responseView.setText("Sent " + threadRunnablesExecutors.this.server
-                                    + ". Message number " + threadRunnablesExecutors.this.messageSent);
-                        }
-                    }
-                });
+            for (int i = 4; i > 0; i --){
+                System.out.println("Thread: " + threadName + " " + i);
+                // thread will sleep for some time
+                Thread.sleep(1000);
             }
-        } catch (Exception e) {
-            final String theErrorMessage = e.getLocalizedMessage();
-            final TextView responseView = TextView.get();
-            uiThreadStack.post(new Runnable() {
-                @Override
-                public void run() {
-                    if (responseView != null) {
-                        responseView.setText("Error: there was an error. " + theErrorMessage);
-                    }
-                }
-            });
+        } catch (InterruptedException ex) {
+            System.out.println("Thread " + threadName + "interrupted.");
         }
+        System.out.println("Thread " + threadName + "existing.");
+    }
+
+    public void start() {
+        System.out.println("Starting" + threadName);
+        if (t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+
     }
 
 }
